348965567b86f31eda39e9b9e28f6ca597fb4b97
Devraj Pal <devved90@gmail.com>
2023-12-02 21:20:05 +0530

    more operations added

--- list.py
+++ list.py
@@ -8,14 +8,51 @@
 # ls1[4] = "Tesla"
 # print(ls1)
 
-ls = ["devraj", "akshay","prashant"]
-# ls.append(999)
-# # ls.sort()
+# ls = ["devraj", "akshay","prashant"]
+# # ls.append(999)
+# # # ls.sort()
+# # print(ls)
+
+# ls.insert(0, "Tesla")
 # print(ls)
 
-ls.insert(0, "Tesla")
-print(ls)
+# # del(ls[2])
 
-# del(ls[2])
+# print(ls[0:1])
+# # print(ls[:])
 
-print(ls[0:1])
+#using fir loop in list`
+ls = [1,2,3,4,45,55,65467,6578,8]
+
+# for i in range(len(ls)):
+#     print(ls[i])
+
+#question is toi find the top 3 maximum elements of the list
+# method 1
+# ls.sort()
+# # print(ls)
+# print(ls[-1],ls[-2], ls[-3])
+
+# way 2
+# print(max(ls))
+
+# way 3
+# maximum = -1
+# for i in ls:
+#     if maximum < i:
+#         maximum = i
+# print(maximum)
+
+
+
+# print 10 times
+# for i in ls:
+#     print(ls)
+
+
+# no 2 data types can be compared
+# for i in ls:
+#     if i < ls:
+#         print(i)
+
+
