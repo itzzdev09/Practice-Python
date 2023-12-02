8889d83a774930b0c517f329dc3a5ca7a380e64c
Devraj Pal <devved90@gmail.com>
2023-12-02 20:35:24 +0530

    git day2 list

---
+++ list.py
@@ -0,0 +1,14 @@
+# ls = ["devraj", "akshay","prashant"]
+# print(ls)
+
+# #indexing in list
+# ls1 = ["devraj", "akshay","prashant", 99, "pal", 46 , True , 99.90]
+# print(ls1[2])
+# print(ls1[-2])
+# ls1[4] = "Tesla"
+# print(ls1)
+
+ls = ["devraj", "akshay","prashant"]
+ls.append(999)
+# ls.sort()
+print(ls)
